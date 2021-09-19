b0d26b213425dc6c5b85038bba848b09dc6b5b73
HHKKsdj <563589126@qq.com>
2021-09-20 00:47:02 +0800

    新增去除文件中注释和字符串的功能，修复了上一版本中的一些问题。

--- main.cpp
+++ main.cpp
@@ -60,10 +60,8 @@ void exclude () {
                 k = line.find("*/")+2;
                 content[i] = line.substr(k,line.length()-k);
             }
-            
         }
     }
-    
 }
 
 void read (string path) {
@@ -82,17 +80,107 @@ void read (string path) {
     fp.close();
 }
 
+void findKeyword () {
+    int num = 0;
+    string line;
+    for (int i = 0; i<content.size(); i++) {
+        line = content[i];
+        for (int j=0; j<32; j++) {
+            size_t fi = line.find(keyword[j], 0);
+            while (fi!=line.npos && keyword[j] != "do")
+            {
+                num++;
+                fi = line.find(keyword[j], fi + 1);
+            }
+        }
+    }
+    cout<<"total num: "<<num<<endl;
+}
+
+void findSwitchCase () {
+    int snum = 0,f = -1,cnum[1000]={0};
+    string line;
+    for (int i = 0; i<content.size(); i++) {
+        line = content[i];
+        if (line.find("switch") != line.npos) {
+            snum += 1;
+            f += 1;
+        }
+        if (line.find("case") != line.npos) {
+            cnum[f] += 1;
+        }
+    }
+    cout<<"switch num: "<<snum<<endl;
+    cout<<"case num:";
+    for (int i = 0; i<=f; i++) {
+        cout<<" "<<cnum[i];
+    }
+    cout<<endl;
+}
+
+void findIfElse (int level) {
+    int sum1 = 0,sum2 = 0;
+    stack<int> stack;
+    string line;
+    for (int i = 0; i<content.size(); i++) {
+        line = content[i];
+        if (line.find("if") != line.npos && line.find("else") == line.npos) { //if
+            stack.push(1);
+        } else if (line.find("if") == line.npos && line.find("else") != line.npos &&  stack.empty() == false) { //else
+            if (stack.top() == 1) {
+                sum1++;
+            } else {
+                sum2++;
+            }
+            stack.pop();
+            
+        }else  if (line.find("if") != line.npos && line.find("else") != line.npos) {  //if else
+            if (stack.top() != 2) {
+                stack.push(2);
+            }
+        }
+    }
+    if (level == 3) {
+        cout<<"if-else num: "<<sum1<<endl;
+    } else if (level == 4) {
+        cout<<"if-else num: "<<sum1<<endl;
+        cout<<"if-else-if num: "<<sum2<<endl;
+    }
+}
+
+
+
 int main(int argc, const char * argv[]) {
+    int level = 0;
     string path,text;
     
-    cin>>path;
+    cin>>path>>level;
     
     read(path);
-    
     exclude();
     
-    for (int i=0; i<content.size(); i++) {
-        cout<<content[i]<<endl;
+    switch (level) {
+        case 1:
+            findKeyword();
+            break;
+        case 2:
+            findKeyword();
+            findSwitchCase();
+            break;
+        case 3:
+            findKeyword();
+            findSwitchCase();
+            findIfElse(3);
+            break;
+        case 4:
+            findKeyword();
+            findSwitchCase();
+            findIfElse(4);
+            break;
+        default:
+            cout<<"Error!"<<endl;
+            break;
     }
+
     return 0;
 }
