729564e72bed2b802221ead9a1f108feea9817ca
HHKKsdj <563589126@qq.com>
2021-09-20 00:40:22 +0800

    新增去除文件中注释和字符串的功能，修复了上一版本中的一些问题。

--- main.cpp
+++ main.cpp
@@ -16,6 +16,56 @@ using namespace::std;
 string keyword[] = {"auto","break","case","char","const","continue","default","do","double","else","enum","extern","float","for","goto","if","int","long","register","return","short","signed","sizeof","static","struct","switch","typedef","union","unsigned","void","volatile","while"};
 vector<string> content;
 
+void exclude () {
+    string text,line,str[4]={"//","\"","/*","*/"};
+    size_t j=0,k=0,t=0,p[50]={0};
+    int i=0,f=0,r=0;
+    for (i=0; i<content.size(); i++) {
+        line = content[i];
+        if (line.find(str[0]) != line.npos) {
+            j = line.find("//");
+            if (j==0) {
+                content.erase(content.begin()+i);
+                i--;
+            } else {
+                text = line.substr(0,j);
+                content[i] = text;
+            }
+        } else if (line.find(str[1]) != line.npos) {
+            while ((t=line.find("\"",k)) != line.npos) {
+                p[f] = t;
+                f++;
+                k=t+1;
+            }
+            p[f] = line.length();
+            text = line.substr(0,p[0]);
+            for (r=1; p[r]!=0; r+=2) {
+                text += line.substr(p[r]+1,p[r+1]-p[r]-1);
+            }
+            content[i] = text;
+        } else if (line.find(str[2]) != line.npos) {
+            j = line.find("/*");
+            k = line.find("*/");
+            
+            if (k != line.npos) {
+                content[i] = line.substr(0,j);
+            } else {
+                content[i] = line.substr(0,j);
+                i += 1;
+                line = content[i];
+                while (line.find("*/") == line.npos) {
+                    content.erase(content.begin()+i);
+                    line = content[i];
+                }
+                k = line.find("*/")+2;
+                content[i] = line.substr(k,line.length()-k);
+            }
+            
+        }
+    }
+    
+}
+
 void read (string path) {
     ifstream fp;
     string text;
@@ -32,81 +82,17 @@ void read (string path) {
     fp.close();
 }
 
-void findKeyword () {
-    int num = 0;
-    string line;
-    for (int i = 0; i<content.size(); i++) {
-        line = content[i];
-        for (int j=0; j<32; j++) {
-            size_t fi = line.find(keyword[j], 0);
-            while (fi!=line.npos && keyword[j] != "do")
-            {
-                num++;
-                fi = line.find(keyword[j], fi + 1);
-            }
-        }
-    }
-    cout<<"total num: "<<num<<endl;
-}
-
-void findSwitchCase () {
-    int snum = 0,f = -1,cnum[1000]={0};
-    string line;
-    for (int i = 0; i<content.size(); i++) {
-        line = content[i];
-        if (line.find("switch") != line.npos) {
-            snum += 1;
-            f += 1;
-        }
-        if (line.find("case") != line.npos) {
-            cnum[f] += 1;
-        }
-    }
-    cout<<"switch num: "<<snum<<endl;
-    cout<<"case num:";
-    for (int i = 0; i<=f; i++) {
-        cout<<" "<<cnum[i];
-    }
-    cout<<endl;
-}
-
-void findIfElse () {
-    int sum1 = 0,sum2 = 0;
-    stack<int> stack;
-    string line;
-    for (int i = 0; i<content.size(); i++) {
-        line = content[i];
-        if (line.find("if") != line.npos && line.find("else") == line.npos) { //if
-            stack.push(1);
-        } else if (line.find("if") == line.npos && line.find("else") != line.npos &&  stack.empty() == false) { //else
-            if (stack.top() == 1) {
-                sum1++;
-            } else {
-                sum2++;
-            }
-            stack.pop();
-            
-        }else  if (line.find("if") != line.npos && line.find("else") != line.npos) {  //if else
-            stack.push(2);
-        }
-    }
-    cout<<"if-else num: "<<sum1<<endl;
-    cout<<"if-else-if num: "<<sum2<<endl;
-}
-
-
-
 int main(int argc, const char * argv[]) {
-    int level = 0;
     string path,text;
     
-    cin>>path>>level;
+    cin>>path;
     
     read(path);
     
-    findKeyword();
-    findSwitchCase();
-    findIfElse();
+    exclude();
     
+    for (int i=0; i<content.size(); i++) {
+        cout<<content[i]<<endl;
+    }
     return 0;
 }
