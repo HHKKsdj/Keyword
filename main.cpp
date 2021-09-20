9f89c2ddac3e3fae4ed36b3bf00981b2de4e7db6
HHKKsdj <563589126@qq.com>
2021-09-20 21:49:43 +0800

    新增行首制表符删除功能，修复了一些bug。

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 #include <vector>
 #include <fstream>
 #include <string>
-#include <regex>
+#include <stack>
 
 using namespace::std;
 
@@ -18,43 +18,56 @@ vector<string> content;
 
 void exclude () {
     string text,line;
-    size_t j=0,k=0,t=0,p[50]={0};
+    size_t j=0,k=0,t=0;
     int i=0,f=0,r=0;
     for (i=0; i<content.size(); i++) {
         line = content[i];
-        if (line.find("//") != line.npos) {
-            j = line.find("//");
+        
+        if (line.find("\t") != line.npos && line.find("\t") == 0) {     /*删除行首制表符*/
+            do {
+                k = line.find("\t")+1;
+                line = line.substr(k,line.length()-k);
+            } while (line.find("\t") != line.npos && line.find("\t") == 0);
+
+            content[i] = line;
+        }
+        if (line.find("//") != line.npos) {     /*查找以“//”开头的注释语句*/
+            j = line.find("//");        /*查找“//”在字符串中的位置*/
             if (j==0) {
-                content.erase(content.begin()+i);
+                content.erase(content.begin()+i);       /*若“//”位于行首则整行删除*/
                 i--;
             } else {
-                text = line.substr(0,j);
+                text = line.substr(0,j);        /*若“//”不位于行首则进行裁切*/
                 content[i] = text;
             }
-        } else if (line.find("\"") != line.npos) {
-            while ((t=line.find("\"",k)) != line.npos) {
+        } else if (line.find("\"") != line.npos) {      /*查找字符串*/
+            size_t p[50] = {0};
+            
+            while ((t=line.find("\"",k)) != line.npos) {        /*查找字符串“"”的位置*/
                 p[f] = t;
                 f++;
                 k=t+1;
             }
+
             p[f] = line.length();
             text = line.substr(0,p[0]);
+            
             for (r=1; p[r]!=0; r+=2) {
-                text += line.substr(p[r]+1,p[r+1]-p[r]-1);
+                text += line.substr(p[r]+1,p[r+1]-p[r]-1);      /*对行中的字符串进行删除*/
             }
             content[i] = text;
-        } else if (line.find("/*") != line.npos) {
+        } else if (line.find("/*") != line.npos) {      /*查找以“/*”开头的注释语句*/
             j = line.find("/*");
             k = line.find("*/");
             
             if (k != line.npos) {
-                content[i] = line.substr(0,j);
+                content[i] = line.substr(0,j);      /*如果注释语句只有一行，则删除注释语句*/
             } else {
                 content[i] = line.substr(0,j);
                 i += 1;
                 line = content[i];
                 while (line.find("*/") == line.npos) {
-                    content.erase(content.begin()+i);
+                    content.erase(content.begin()+i);       /*如果有多行注释语句，则逐行删除*/
                     line = content[i];
                 }
                 k = line.find("*/")+2;
@@ -88,14 +101,15 @@ bool judge(string l, string s){
             return false;
         }
     }
+    
 }
 
 void read (string path) {
     ifstream fp;
     string text;
     
-    fp.open(path,ios::in);
-    if (!fp.is_open()) {
+    fp.open(path,ios::in);      /*打开文件*/
+    if (!fp.is_open()) {        /*逐行读取文件内容*/
         cout<<"Error!"<<endl;
         return;
     }
@@ -103,7 +117,7 @@ void read (string path) {
     while (getline(fp, text)) {
         content.push_back(text);
     }
-    fp.close();
+    fp.close();     /*关闭文件*/
 }
 
 void findKeyword () {
@@ -127,11 +141,11 @@ void findSwitchCase () {
     string line;
     for (int i = 0; i<content.size(); i++) {
         line = content[i];
-        if (line.find("switch") != line.npos) {
+        if (line.find("switch") != line.npos && judge(line, "switch")) {        /*查找switch*/
             snum += 1;
             f += 1;
         }
-        if (line.find("case") != line.npos) {
+        if (line.find("case") != line.npos && judge(line, "case")) {        /*查找case*/
             cnum[f] += 1;
         }
     }
@@ -145,24 +159,22 @@ void findSwitchCase () {
 
 void findIfElse (int level) {
     int sum1 = 0,sum2 = 0;
-    stack<int> stack;
+    stack<int> s;
     string line;
     for (int i = 0; i<content.size(); i++) {
         line = content[i];
-        if (line.find("if") != line.npos && line.find("else") == line.npos && judge(line,"if")) { //if
-            stack.push(1);
-        } else if (line.find("if") == line.npos && line.find("else") != line.npos &&  stack.empty() == false) { //else
-            if (stack.top() == 1) {
-                sum1++;
+        if (line.find("if") != line.npos && line.find("else") == line.npos && judge(line, "if")) {      /*遇到if时将1入栈*/
+            s.push(1);
+        } else if (line.find("if") == line.npos && line.find("else") != line.npos &&  s.empty() == false) {
+            if (s.top() == 1) {
+                sum1++;     /*若栈顶为1则与 if else 结构匹配成功*/
             } else {
-                sum2++;
+                sum2++;     /*若栈顶为2则与 if else_if else 结构匹配成功*/
             }
-            stack.pop();
+            s.pop();        /*出栈*/
             
-        }else  if (line.find("if") != line.npos && line.find("else") != line.npos && judge(line,"if")) {  //if else
-            if (stack.top() != 2) {
-                stack.push(2);
-            }
+        }else  if (line.find("if") != line.npos && line.find("else") != line.npos && judge(line, "if")) {
+            s.push(2);      /*遇到if_else时则将2入栈*/
         }
     }
     if (level == 3) {
